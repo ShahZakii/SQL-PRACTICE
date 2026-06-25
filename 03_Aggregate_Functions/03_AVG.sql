7014bc13643d7bb4e99d555e8a5ab875b5135ca7
MOHD ZAKI SHAH SAIFI <shahhzaki@gmail.com>
2026-06-25 22:58:53 +0530

    ADDED AVERAGE FUNCTION IN SQL
    Added SQL examples demonstrating the use of the AVG() aggregate function, including syntax, usage with WHERE, GROUP BY, and DISTINCT.

---
+++ 03_Aggregate_Functions/03_AVG.sql
@@ -0,0 +1,52 @@
+-- Definition:-
+
+-- AVG() is an SQL aggregate function that calculates the average value of a numeric column.
+
+-- It is commonly used to find:
+
+-- 1. Average salary
+-- 2. Average marks
+-- 3. Average product price
+-- 4. Average sales amount
+
+-- Syntax:
+SELECT AVG(column_name)
+FROM table_name;
+
+-- Example:
+SELECT AVG(salary)
+FROM employees;
+
+-- This returns the average salary of all employees.
+
+-- Using with WHERE:
+SELECT AVG(salary)
+FROM employees
+WHERE department = 'IT';
+
+-- Average salary of employees in the IT department.
+
+-- Using with GROUP BY:
+SELECT department, AVG(salary)
+FROM employees
+GROUP BY department;
+
+-- Average salary for each department.
+
+-- Using with ROUND:
+SELECT ROUND(AVG(salary), 2)
+FROM employees;
+
+-- Rounds the average to 2 decimal places.
+
+-- AVG with DISTINCT:
+SELECT AVG(DISTINCT salary)
+FROM employees;
+
+-- Calculates average of unique salary values only
+
+-- Practice Example:
+SELECT department, ROUND(AVG(salary), 1) AS avg_salary
+FROM employees
+GROUP BY department
+HAVING AVG(salary) > 50000;
